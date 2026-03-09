cdf0419c24cc5b9e4515b379a872102790496417
Catherine Cancino <catherine_cancino@dlsu.edu.ph>
2026-03-09 13:54:29 +0800

    Added config for APScheduler

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
-
+import os
 
 class ApiConfig(AppConfig):
-    name = 'api'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api' # Make sure this matches your app name
+
+    def ready(self):
+        # This prevents the scheduler from running twice when Django's auto-reloader is active
+        if os.environ.get('RUN_MAIN') == 'true':
+            from .scheduler import start_watchdog
+            start_watchdog()
+            print("🟢 IoT Star Topology Watchdog Started!")
